44c1b83098582264f387bb963ecca939f00b7906
Ebuka-Francis <nwokefrancis99@gmail.com>
2023-11-06 11:30:26 +0000

    A new project

---
+++ main.js
@@ -0,0 +1,35 @@
+let myFrom = document.querySelector("#form");
+let firstName = document.querySelector("#firstname");
+let lastName = document.querySelector("#lastname");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+let msg = document.querySelector(".msg");
+let submit = document.querySelector("#submit");
+
+myFrom.addEventListener('submit', myFunction);
+
+function myFunction(callback){
+    callback.preventDefault()
+
+    if(firstName.value === "") {
+        alert("Firstname can not be blank")
+    }
+    if(lastName.value === ''){
+        alert("Lastname cannot be blank")
+    }
+    if(email.value === '' ){
+        alert("input email")
+    }
+    if(password.value === '') {
+        alert("password cannot be empty")
+    } 
+    if(password.value !== confirmPassword.value) {
+        alert("password not the same")
+    }else {
+
+    }
+
+
+}
+
